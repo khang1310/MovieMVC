b326842a1ef2791832a6fd9191c934356c0adb2f
Khang Nguyen <khang13102000@gmail.com>
2023-08-15 16:04:58 -0400

    First commit

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieWebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        [HttpGet("/HelloWorld")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/HelloWorld/Welcome/{name}/{id}")]
+        public IActionResult Welcome(string name, int id = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["ID"] = id;
+
+            return View();
+        }
+    }
+}
